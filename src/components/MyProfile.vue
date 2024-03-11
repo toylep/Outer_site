5f0b6cf0ffdb378596c428e843c7e692db168912
R321 <troltoh@gmail.com>
2024-03-11 20:44:35 +0800

    ryga

--- src/components/MyProfile.vue
+++ src/components/MyProfile.vue
@@ -1,21 +1,24 @@
 <template>
     <h1>Профиль</h1>
-        <div class="card" >
-            <div class="card-body">
-                <h3 class="card-title">{{userStorage.partner.image}}</h3>
-                <hr>
-                <h3 class="card-title">Название организации: {{userStorage.partner.username}}</h3>
-                <hr>
-                <h3 class="card-title">Договор: {{userStorage.partner.agreement}}</h3>
-                <hr>
-                <h3 class="card-title">Названия практик: {{userStorage.partner.practice_topics}}</h3>  
-                <hr>
-                
-
-
-            </div>
+    <div class="card">
+        <div class="card-body">
+            <h3 class="card-title">{{ userStorage.partner.image }}</h3>
+            <hr>
+            <h3 class="card-title">Название компании: {{ userStorage.partner.username }}</h3>
+            <hr>
+            <h3 class="card-title">Договор: {{ userStorage.partner.agreement }}</h3>
+            <hr>
+            <h3 class="card-title">Темы заданий на практику: {{ userStorage.partner.practice_topics }}</h3>
+            <hr>
+            <hr>
+            <h3 class="card-title">Сфера деятельности: {{ userStorage.partner }}</h3>
+            <hr>
+            <hr>
+            <h3 class="card-title">Контактное лицо: {{ userStorage.partner }}</h3>
+            <hr>
         </div>
-        
+    </div>
+
 </template>
 <script setup>
 import { ref } from 'vue'
@@ -23,33 +26,33 @@ import { useUserStorage } from '@/storages/UserStorage';
 import { onBeforeMount } from 'vue'
 import axios from 'axios'
 const userStorage = ref(useUserStorage())
-let change_button =ref(true)
-const changeUser = async () =>{
+let change_button = ref(true)
+const changeUser = async () => {
     console.log(userStorage.value.id)
-    await axios.patch('/api/users/'+ userStorage.value.id ,
+    await axios.patch('/api/users/' + userStorage.value.id,
     )
-    .then(res =>userStorage.value.setAuth({
-        username:userHolder.username,
-        password:userStorage.value.password
-    }))
-    .then(res => userStorage.value.setUserFromServer())
+        .then(res => userStorage.value.setAuth({
+            username: userHolder.username,
+            password: userStorage.value.password
+        }))
+        .then(res => userStorage.value.setUserFromServer())
 }
-const on_change_click = () =>{
-    change_button.value*=-1
+const on_change_click = () => {
+    change_button.value *= -1
 }
 const userHolder = {
     image: '',
-	username: '',
-	agreement: '',
+    username: '',
+    agreement: '',
     practice_topics: ''
 
 
 }
 
 onBeforeMount(() => {
-	if (!(JSON.parse(localStorage.getItem('user')) === null)){
-		userStorage.value.setUser(JSON.parse(localStorage.getItem('user')))
-		userStorage.value.setAuth(JSON.parse(localStorage.getItem('auth')))
-	}
+    if (!(JSON.parse(localStorage.getItem('user')) === null)) {
+        userStorage.value.setUser(JSON.parse(localStorage.getItem('user')))
+        userStorage.value.setAuth(JSON.parse(localStorage.getItem('auth')))
+    }
 })
 </script>@/storages/UserStorage
--- src/storages/UserStorage.js
+++ src/storages/UserStorage.js
@@ -18,35 +18,35 @@ export const useUserStorage = defineStore('userStore',{
     }),
     actions:{
        
-        setUser(partner){
-            this.partner=partner
-        },
-        setAuth(auth){
-            this.auth=auth
-        },
-        async setUserFromServer(){
-            this.auth.username = JSON.parse(localStorage.getItem('auth')).username
-            if (this.auth != null){
-                await axios.get(
-                    '/api/users/'+this.auth.username
-                ).then(res=>res.data)
-                 .then(data=>{
-                    this.user=data
-                    localStorage.setItem('user', JSON.stringify(data));
-                 })
-            }
-            else alert('Вы не авторизованы')
-        },
-        async getUserList(){
-            axios.get('/api/users/')
-            .then(resp=>resp.data)
-            .then(data=>this.users=data)
-        },
-        async changeAuthority(id){
-            axios.patch('api/users/'+id)
-            .then(resp=>resp.data)
-            .then(data => this.users.filter((el)=>el.id === data.id)[0] = data)
-        }
+        // setUser(partner){
+        //     this.partner=partner
+        // },
+        // setAuth(auth){
+        //     this.auth=auth
+        // },
+        // async setUserFromServer(){
+        //     this.auth.username = JSON.parse(localStorage.getItem('auth')).username
+        //     if (this.auth != null){
+        //         await axios.get(
+        //             '/api/users/'+this.auth.username
+        //         ).then(res=>res.data)
+        //          .then(data=>{
+        //             this.user=data
+        //             localStorage.setItem('user', JSON.stringify(data));
+        //          })
+        //     }
+        //     else alert('Вы не авторизованы')
+        // },
+        // async getUserList(){
+        //     axios.get('/api/users/')
+        //     .then(resp=>resp.data)
+        //     .then(data=>this.users=data)
+        // },
+        // async changeAuthority(id){
+        //     axios.patch('api/users/'+id)
+        //     .then(resp=>resp.data)
+        //     .then(data => this.users.filter((el)=>el.id === data.id)[0] = data)
+        // }
         
 
     }
