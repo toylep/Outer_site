3a3f381f8c4b6c1362f5e8e2ad378592d7195fdf
R321 <troltoh@gmail.com>
2024-03-18 09:53:23 +0800

    test3

--- src/storages/InstStorage.js
+++ src/storages/InstStorage.js
@@ -8,7 +8,7 @@ export const useInstStorage = defineStore("instituteStore", {
   actions: {
     async getInstsFromServer() {
       console.log("получаем институты")
-      axios.get("/api/out/faculty/legacy/")
+      axios.get("/api/out/legacy/faculty")
       .then((response) => {
           this.Insts = response.data;
           console.log(this.Insts);
