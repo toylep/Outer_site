27f2c25b07c7ac2f0713e83e6c33dbf2df1ada6c
redhead <troltoh@gmail.com>
2024-04-01 14:24:18 +0800

    as

--- src/components/Hat.vue
+++ src/components/Hat.vue
@@ -14,11 +14,11 @@
             <a href="https://t.me/ckirnitu"><button class="hat_butt"><img
                         src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/1200px-Telegram_logo.svg.png"
                         style="height: 35px; width: 35px; "></button></a>
-            <RouterLink to= "/in"><button class="hat_butt"><img
+            <a href to= "/in"><button class="hat_butt"><img
                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRALBplMxySL51Y_dJZ5FTrLpa1rpD72FebJA&usqp=CAU"
                     style="height: 35px; width: 35px; margin-left: 7px;">
                 
-            </button></RouterLink>
+            </button></a>
         </div>
     </div>
 
